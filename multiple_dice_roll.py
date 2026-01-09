d60cd20919e55af643789d1ccc4fc4c397ca4eb6
Sahithrv360 <rvsahithreddy@gmail.com>
2026-01-09 17:54:26 +0530

    created for multiple die and written ascii codes for symbols

--- multiple_dice_roll.py
+++ multiple_dice_roll.py
@@ -1,7 +1,7 @@
 import random
 
 ''' ● ┌ ─ ┐ │ └ ┘ '''
-'''Ascii codes for above symbols : \u25CF \u250C \u2500 \u2510 \u2502 \u2514 \u2518 '''
+''' \u25CF '''
 roll = ( ('┌─────────┐',
           '│         │',
           '│    ●    │',
@@ -34,15 +34,16 @@ roll = ( ('┌─────────┐',
           '└─────────┘' ) )
 
 roll_die = True
-
+number_of_dice = int(input('Number of dices do you want to roll at a time : '))
 while roll_die:
-    player = input('Do you want to roll the die (y/n) : ')
+    player = input('Do you want to roll the dice (y/n) : ')
     if player.lower() == 'y':
-        number = random.choice(roll)
-        for val in number:
-            print(val)
+        for _ in range(number_of_dice):
+            number = random.choice(roll)
+            for val in number:
+                print(val)
     elif player.lower() == 'n':
-        print('Thank you for using the die')
+        print('Thank you for using the dice')
         roll_die = False
     else :
         print('Enter either y/n !!!')
